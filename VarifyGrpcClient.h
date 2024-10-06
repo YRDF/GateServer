9b51b0fdec5d88a9a65e58c9e38da847ff33f76c
YRDF <zzh5653@163.com>
2024-10-06 19:38:42 +0800

    更改默认ip

--- VarifyGrpcClient.h
+++ VarifyGrpcClient.h
@@ -35,7 +35,7 @@ public:
 
 private:
     VarifyGrpcClient() {
-        std::shared_ptr<Channel> channel = grpc::CreateChannel("0.0.0.0:50051", 
+        std::shared_ptr<Channel> channel = grpc::CreateChannel("127.0.0.1:50051", 
             grpc::InsecureChannelCredentials());
         stub_ = VarifyService::NewStub(channel);
     }
